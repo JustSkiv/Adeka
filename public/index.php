24fb55cc2f63490f897ce1b853a1d2ff4a652eb4
Nikolay Tuzov <just.skiv@gmail.com>
2017-07-22 14:00:35 +0300

    init core

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by Nikolay Tuzov
+ */
+
+$query = $_SERVER['QUERY_STRING'];
+
+require '../vendor/core/Router.php';
+
+$router = new Router();
---
+++ vendor/core/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by Nikolay Tuzov
+ */
+class Router
+{
+    public function __construct()
+    {
+        echo 'test фыв';
+    }
+}
