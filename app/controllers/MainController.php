e57713d68feae7bb571b3675a821d4922bc3f867
Nikolay Tuzov <tuzov.n@gmail.com>
2017-07-27 08:09:23 +0300

    мелкая правка

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -24,8 +24,6 @@ class MainController extends AppController
 
         $this->setData([
             'title' => 'Main page title',
-            'windows' => $windows,
-//            'windowTest' => $windowTest
         ]);
     }
 
